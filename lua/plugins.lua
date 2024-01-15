b0ec7f040b1531b22286e7a1a4d6b3fa7a124a85
AidasJur <aidas.jurelevicius@gmail.com>
2024-01-15 09:26:38 +0200

    Integrated Undotree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -35,6 +35,14 @@ return {
     },
 
     {
+        'mbbill/undotree',
+        config = function ()
+            vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle, { desc = "[U]ndotree [T]oggle" })
+            vim.keymap.set("n", "<leader>uf", vim.cmd.UndotreeFocus, { desc = "[U]ndotree [F]ocus" })
+        end,
+    },
+
+    {
         -- Autocompletion
         'hrsh7th/nvim-cmp',
         dependencies = {
