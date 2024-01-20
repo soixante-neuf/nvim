220629b4a0af883d05db50ff7062c09500ef0044
AidasJur <aidas.jurelevicius@gmail.com>
2024-01-21 00:49:19 +0200

    Added fugitive

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -34,6 +34,14 @@ return {
     },
 
     {
+        "tpope/vim-fugitive",
+        event = "VeryLazy",
+        config = function ()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
+        end
+    },
+
+    {
         -- Autocompletion
         'hrsh7th/nvim-cmp',
         dependencies = {
