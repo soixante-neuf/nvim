4cd97273c7b9ef1abf3e6034803e5e6a85a562c6
AidasJur <aidas.jurelevicius@gmail.com>
2024-01-20 19:36:47 +0200

    Fixed snippet crashing

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,41 +22,43 @@ return {
                 },
             })
 
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-            capabilities.textDocument.completion.completionItem.snippetSupport = false
-
             require("neodev").setup()
 
-            require("mason-lspconfig").setup_handlers {
+            require("mason-lspconfig").setup_handlers({
                 function(server_name)
                     require('lspconfig')[server_name].setup({
-                        capabilities = capabilities,
+                        capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol
+                        .make_client_capabilities())
                     })
                 end,
-            }
+            })
 
             local cmp = require('cmp')
             cmp.setup({
                 sources = cmp.config.sources({
-                    { name = 'nvim_lsp', group_index = 1 },
-                    { name = 'buffer',   group_index = 2 },
-                    { name = 'path',     group_index = 3 },
+                    {
+                        name = 'nvim_lsp',
+                        group_index = 1,
+                        entry_filter = function(entry, _)
+                            -- get_kind == 15 when entry is a snippet
+                            if entry:get_kind() == 15 then
+                                return false
+                            end
+                            return true
+                        end,
+                    },
+                    { name = 'buffer', group_index = 2 },
+                    { name = 'path',   group_index = 3 },
                 }),
                 preselect = 'item',
                 completion = {
-                    completeopt = 'menu,menuone,noinsert'
+                    completeopt = 'menu,menuone,noinsert,noselect,preview'
                 },
                 mapping = cmp.mapping.preset.insert({
                     ["<C-p>"] = cmp.mapping.select_prev_item(),
                     ["<C-n>"] = cmp.mapping.select_next_item(),
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
-
-                    ['<Tab>'] = cmp.mapping.confirm({
-                        select = false,
-                        behavior = cmp.ConfirmBehavior.Insert,
-                    }),
                 }),
 
             })
