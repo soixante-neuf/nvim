18086c9c85a52af896eea33bed104d34feb22c86
AidasJur <aidas.jurelevicius@gmail.com>
2024-01-23 18:13:05 +0200

    Updated autocompletion

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -48,6 +48,8 @@ return {
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-buffer',
+
+            "L3MON4D3/LuaSnip",
         },
     },
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,22 +35,21 @@ return {
                 end,
             })
 
+            local luasnip = require('luasnip')
+            require('luasnip.loaders.from_vscode').lazy_load()
+            luasnip.config.setup({})
+
             local cmp = require('cmp')
             cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
                 sources = cmp.config.sources({
-                    {
-                        name = 'nvim_lsp',
-                        group_index = 1,
-                        entry_filter = function(entry, _)
-                            -- get_kind == 15 when entry is a snippet
-                            if entry:get_kind() == 15 then
-                                return false
-                            end
-                            return true
-                        end,
-                    },
-                    { name = 'buffer', group_index = 2 },
-                    { name = 'path',   group_index = 3 },
+                    { name = 'nvim_lsp', group_index = 1 },
+                    { name = 'buffer',   group_index = 2 },
+                    { name = 'path',     group_index = 3 },
                 }),
                 preselect = 'item',
                 completion = {
@@ -61,8 +60,12 @@ return {
                     ["<C-n>"] = cmp.mapping.select_next_item(),
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                }),
 
+                    ['<Tab>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    },
+                }),
             })
         end
     },
