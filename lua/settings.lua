f5b6cad2a61d334dfd548a60aceeb1907239f4ee
AidasJ21 <aidas.jurelevicius@gmail.com>
2024-09-04 00:28:06 +0300

    Fixed few things

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,13 +1,18 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.mouse = a
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
+vim.opt.cursorline = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
@@ -18,8 +23,6 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
